6d2056c70aa44f59816f30ec470adfb5b736955e
William Mock <will@mockdesign.com>
2017-04-26 12:07:19 -0500

    changes to graphs and images

--- ThompsonTracker/ThompsonTracker/GraphView.swift
+++ ThompsonTracker/ThompsonTracker/GraphView.swift
@@ -16,6 +16,10 @@ import UIKit
     
     var graphPoints = [Int]()
     
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func draw(_ rect: CGRect) {
         
         //print(graphPoints)
